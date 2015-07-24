46a48d8bb62cd230f382f9a8e7f5367d34f5d932
yukikoohara <yukiko19811123@gmail.com>
2015-07-24 10:37:07 +0900

    app/Controller/PostsController.php app/Controller/CategoriesController.phpの中身 app/View/Categories/category_index.ctp app/View/Posts/add.ctp                          /edit.ctp                          /index.ctp                          /view.ctp 変更しました！

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -1,13 +1,8 @@
 <?php
 class ContegoriesController extends AppController {
-	public function category_index1 (){
+	public function category_index (){
 		
 	}
-	public function category_index2 (){
-		
-	}
-	public function category_index3 (){
-
-	}
+	
 }
 ?>
--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,43 +1,21 @@
 <?php
 class PostsController extends AppController {
-	public function index1(){
+	public function index(){
 
 	}
-	public function add1(){
+	public function add(){
 
 	}
-	public function view1(){
+	public function view(){
 
 	}
-	public function edit1(){
+	public function edit(){
+
+	}
+	public function delete(){
 
 	}
 
-
-	public function index2(){
-
-	}
-	public function add2(){
-
-	}
 	
-	public function view2(){
-
-	}
-	public function edit2(){
-
-	}
-	public function index3(){
-
-	}
-	public function add3(){
-
-	}
-	public function view3(){
-
-	}
-	public function edit3(){
-		
-	}
 }
 ?>
